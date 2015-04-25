76580b0c9684205f152897da92332a3dd0638bf1
Andrew Siemer <asiemer@hotmail.com>
2015-04-25 12:44:44 -0500

    added shortbus in infrastructure, and API, structuremap to infra, web, and api

---
+++ src/Infrastructure/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShortBus;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace Infrastructure
+{
+    public class Bootstrapper
+    {
+        public void Start()
+        {
+            ObjectFactory.Initialize(i => i.Scan(s =>
+            {
+                s.AssemblyContainingType<IMediator>();
+                s.TheCallingAssembly();
+                s.WithDefaultConventions();
+                s.AddAllTypesOf(typeof(IQueryHandler<,>));
+                s.AddAllTypesOf(typeof(ICommandHandler<>));
+            }));   
+        }
+    }
+}
